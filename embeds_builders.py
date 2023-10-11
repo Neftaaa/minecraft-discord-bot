7d24418617d2d589d9213b3b71f95e3c3d943137
nefta <theo.richard.delmas@gmail.com>
2023-10-11 15:21:34 +0200

    Updated embeds builders functions

--- embeds_builders.py
+++ embeds_builders.py
@@ -6,17 +6,28 @@ def convert_server_info_dict(server_info: dict) -> dict:
     motd = server_info["motd"]
     clean_motd = motd["clean"]
     if len(clean_motd) == 1:
-        final_motd = clean_motd[0]
+        usable_motd = clean_motd[0]
     else:
-        final_motd = f"{clean_motd[0]}\n{clean_motd[1]}"
+        usable_motd = f"{clean_motd[0]}\n{clean_motd[1]}"
 
     players_info = server_info["players"]
     online_player_count = players_info["online"]
     max_player_count = players_info["max"]
 
     version = server_info["version"]
-    return {"final_motd": final_motd, "online_player_count": online_player_count,
+    return {"final_motd": usable_motd, "online_player_count": online_player_count,
             "max_player_count": max_player_count, "version": version}
+
+
+def build_help_embed():
+    help_embed = discord.Embed(title="Help", color=discord.Color.green())
+    help_embed.add_field(name="Bot information:", value="```MC Server Info is a discord bot that returns information about a minecraft server.```", inline=False)
+    help_embed.add_field(name="/help", value="```Show this message.```", inline=False)
+    help_embed.add_field(name="/serverinfo `address`", value="```Will return information about a specific minecraft server. Can be use without specifying the address if you set"
+                                                             " a default address with '/setdefault'.```", inline=False)
+    help_embed.add_field(name="/setdefault `default_address`", value="```Set a default address that the bot will use if you don't specify an address using '/serverinfo'.```",
+                         inline=False)
+    return help_embed
 
 
 def build_online_server_embed(server_info: dict, address, favicon_url: str) -> (discord.embeds.Embed, None):
@@ -27,12 +38,12 @@ def build_online_server_embed(server_info: dict, address, favicon_url: str) -> (
     server_info_embed.set_author(name=address, icon_url=favicon_url)
     server_info_embed.set_thumbnail(url=favicon_url)
 
-    server_info_embed.add_field(name="MOTD:", value=usable_server_info["final_motd"], inline=False)
+    server_info_embed.add_field(name="MOTD:", value=f"```{usable_server_info['final_motd']}```", inline=False)
     server_info_embed.add_field(name="\u200B", value="\u200B", inline=False)
-    server_info_embed.add_field(name="Status:", value=f"Online", inline=True)
-    server_info_embed.add_field(name="Player(s):", value=f"{usable_server_info['online_player_count']}"
-                                                         f"/{usable_server_info['max_player_count']}", inline=True)
-    server_info_embed.add_field(name="Version:", value=usable_server_info['version'], inline=True)
+    server_info_embed.add_field(name="Status:", value=f"`Online`", inline=True)
+    server_info_embed.add_field(name="Player(s):", value=f"`{usable_server_info['online_player_count']}"
+                                                         f"/{usable_server_info['max_player_count']}`", inline=True)
+    server_info_embed.add_field(name="Version:", value=f"`{usable_server_info['version']}`", inline=True)
 
     server_info_embed.timestamp = datetime.now()
     server_info_embed.set_footer(text="\u200B", icon_url=favicon_url)
@@ -42,9 +53,9 @@ def build_online_server_embed(server_info: dict, address, favicon_url: str) -> (
 def build_unreachable_server_embed(address) -> (discord.embeds.Embed, discord.File):
     error_image = discord.File("resources/error.png", filename="error.png")
 
-    server_info_embed = discord.Embed(title="Can't connect to server.", color=discord.Color.green())
+    server_info_embed = discord.Embed(title="Can't connect to server.", color=discord.Color.red())
 
-    server_info_embed.add_field(name="", value="Server is offline or doesn't exist.")
+    server_info_embed.add_field(name="", value="`Server is offline or doesn't exist.`")
 
     server_info_embed.set_author(name=address, icon_url="attachment://error.png")
     server_info_embed.set_thumbnail(url="attachment://error.png")
@@ -64,14 +75,3 @@ def build_default_address_embed(default_address: str) -> (discord.embeds.Embed, 
     default_address_embed.timestamp = datetime.now()
     default_address_embed.set_footer()
     return default_address_embed, valid_image
-
-
-def build_help_embed():
-    help_embed = discord.Embed(title="Help", color=discord.Color.green())
-    help_embed.add_field(name="Bot information:", value="MC Server Info is a discord bot that returns information about a minecraft server.", inline=False)
-    help_embed.add_field(name="/help", value="Show this message.", inline=False)
-    help_embed.add_field(name="/serverinfo 'address'", value="Will return information about a specific minecraft server. Can be use without specifying the address if you set"
-                                                             " a default address with '/setdefault'.", inline=False)
-    help_embed.add_field(name="/setdefault 'default_address'", value="Set a default address that the bot will use if you don't specify an address using '/serverinfo'.",
-                         inline=False)
-    return help_embed
